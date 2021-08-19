5d135fcd912cd27f63792313ad9f167c146f83da
rahmatberl <rahmatberlianda@gmail.com>
2021-08-19 13:16:14 +0700

    General Page

---
+++ lib/ui/pages/pages.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:foodmarket/shared/shared.dart';
+import 'package:supercharged/supercharged.dart';
+
+part 'general_page.dart';
