dab4f01b78423a8c626b0e4af02dce834f26174a
rahmatberl <rahmatberlianda@gmail.com>
2021-08-19 16:17:05 +0700

    Sign Up

--- lib/shared/shared.dart
+++ lib/shared/shared.dart
@@ -1,1 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:supercharged/supercharged.dart';
+
 part 'theme.dart';
--- lib/ui/pages/general_page.dart
+++ lib/ui/pages/general_page.dart
@@ -25,18 +25,53 @@ class GeneralPage extends StatelessWidget {
           ),
           SafeArea(
               child: Container(
-            color: backColor ?? "FAFAFC".toColor(),
+            color: backColor ?? Colors.white,
           )),
           SafeArea(
             child: ListView(children: [
               Column(
                 children: [
                   Container(
-                    margin: EdgeInsets.only(bottom: defaultMargin),
                     padding: EdgeInsets.symmetric(horizontal: defaultMargin),
                     width: double.infinity,
                     height: 100,
                     color: Colors.white,
+                    child: Row(
+                      children: [
+                        onBackbbuttonPressed != null
+                            ? Container(
+                                width: defaultMargin,
+                                height: defaultMargin,
+                                margin: EdgeInsets.only(right: 26),
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            'assets/back_arrow.png'))))
+                            : SizedBox(),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              title,
+                              style: GoogleFonts.poppins(
+                                  fontSize: 22, fontWeight: FontWeight.w500),
+                            ),
+                            Text(
+                              subtitle,
+                              style: GoogleFonts.poppins(
+                                  color: "8D92A3".toColor(),
+                                  fontWeight: FontWeight.w300),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: defaultMargin,
+                    width: double.infinity,
+                    color: "FAFAFC".toColor(),
                   ),
                   child ?? SizedBox()
                 ],
